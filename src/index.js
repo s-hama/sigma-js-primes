279a95cd0a6ce62a3bcafb8952ba5ee212b23dae
unisia-se <s-hama@unisia-se.jp>
2023-08-28 21:02:33 +0900

    refactor: arrow function refactor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 // Generate a list of prime numbers using the Sieve of Eratosthenes.
 let maxInt = 8388607;
 let primes = null;
-generatePrimes();
-
-// Generate a list of prime numbers in the target range.
-function generatePrimes() {
+const generatePrimes = () => {
+  // Generate a list of prime numbers in the target range.
   primes = new Array(maxInt + 1).fill(true);
   primes[0] = primes[1] = false;
   for (let i = 2; i * i <= maxInt; i++) {
@@ -14,24 +12,25 @@ function generatePrimes() {
       }
     }
   }
-}
+};
+generatePrimes();
 
 // Re-generate the list of primes in the target range by re-setting the maximum value.
-function changeMaxInt(newMaxInt) {
+const changeMaxInt = (newMaxInt) => {
   if (newMaxInt < 1) throw new Error("Maximum integer must be greater than 0.");
   maxInt = newMaxInt;
   generatePrimes();
-}
+};
 
 // Determine prime numbers.
-function isPrime(n) {
-  if (n < 2) return false;
-  if (n > maxInt)
+const isPrime = (num) => {
+  if (num < 2) return false;
+  if (num > maxInt)
     throw new Error(
-      `The value exceeds the maximum of ${maxInt}. Please specify an integer greater than or equal to 2 and less than or equal to ${maxInt}.`
+      `The value exceeds the maximum of ${maxInt}.Please specify an integer greater than or equal to 2 and less than or equal to ${maxInt}.`
     );
-  return primes[n];
-}
+  return primes[num];
+};
 
 // Export module.
 module.exports = {
