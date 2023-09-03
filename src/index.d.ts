e8d6c9203a62fd3829d1cc2b697583129c6496cf
unisia-se <s-hama@unisia-se.jp>
2023-09-03 19:45:26 +0900

    refactor: optimize replacement strings (add getMsg function)

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,11 +3,9 @@ declare module "@sigma-js/primes" {
   type IsPrime = (num: number) => boolean;
   type GetPrimes = (start?: number, end?: number) => number[];
   type GetFactors = (num: number) => number[];
-  type Msgs = {[key: string]: string};
 
   export const changeMaxInt: ChangeMaxInt;
   export const isPrime: IsPrime;
   export const getPrimes: GetPrimes;
   export const getFactors: GetFactors;
-  export const msgs: Msgs;
 }
